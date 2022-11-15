e56991f6289d6c23dec2dddefa597be1c2f0a359
chaitanya44444 <93338022+chaitanya44444@users.noreply.github.com>
2022-11-15 16:12:36 +0530

    Create 6546565.py

---
+++ 6546565.py
@@ -0,0 +1,9 @@
+kilometers = float(input("Please Enter the Kilometers = "))
+
+meter = kilometers * 1000
+centimeter = kilometers * 100000
+millimeter = kilometers * 1000000
+ 
+print(kilometers," Kilometers = ",meter," Meters")
+print(kilometers,'Kilometers =' ,centimeter,' Centimeters')
+print(kilometers,' Kilometers = ',millimeter,' Millimeters')
